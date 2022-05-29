2e6f764722b459bd57ae429945ff3a1cafc8fbfe
Otoshigami <106455952+Otoshigami@users.noreply.github.com>
2022-05-28 19:44:31 -0500

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,20 @@
+
+function login() {
+    
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("pass_field").value;
+    // window.alert(userEmail + "   " + userPass);
+
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+        .then((userCredential) => {
+            // Signed in
+            var user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            window.alert("Error: " + errorMessage);
+        });
+
+}
